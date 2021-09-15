8b49b0ec65e9f559d1b9e9f0d669a0a38301e596
Benjamin Remy <remy.benjamin.101@gmail.com>
2021-09-15 15:43:57 +0200

    add PSF conv

--- toymodel1.py
+++ toymodel1.py
@@ -1,6 +1,7 @@
 import edward2 as ed
 import tensorflow as tf
 import numpy as np
+import galsim
 import matplotlib.pyplot as plt
 from absl import app
 
@@ -29,22 +30,40 @@ def model(stamp_size):
   log_l_hlr = ed.Normal(loc=-.68, scale=.3)
   hlr = tf.math.exp(log_l_hlr * _log10)
 
-  # prior on intensity
-  #log_l_I = ed.Normal(loc=-1.97, scale=.53)
-  #I = tf.math.exp(log_l_I * _log10)
+  # PSF model from galsim COSMOS catalog
+  cat = galsim.COSMOSCatalog()
+  psf = cat.makeGalaxy(2,  gal_type='real', noise_pad_size=0).original_psf
+
+  interp_factor=2
+  padding_factor=2
+  Nk = stamp_size*interp_factor*padding_factor
+  from galsim.bounds import _BoundsI
+  bounds = _BoundsI(0, Nk//2, -Nk//2, Nk//2-1)
+
+  imkpsf = psf.drawKImage(bounds=bounds,
+                          scale=2.*np.pi/(stamp_size*padding_factor*_scale),
+                          recenter=False)
+
+  kpsf = tf.cast(np.fft.fftshift(imkpsf.array.reshape(1, Nk, Nk//2+1), axes=1), tf.complex64)
 
   # Flux
   F = 16.693710205567005
-  #b_n = lp.calculate_b(n)
-  #F = 2 * _pi * tf.math.exp(tf.math.lgamma(2*n)) * tf.math.exp(b_n) * hlr  * hlr * I / tf.math.pow(b_n, 2*n)
 
   # generate light profile
   profile = lp.sersic(n, scale_radius=hlr, flux=F, nx=nx, ny=ny, scale=_scale)
+
+  # Convolve the image with the PSF
+  ims = tf.cast(tf.reshape(profile, (1,stamp_size,stamp_size,1)), tf.float32)
+  profile = galflow.convolve(ims, kpsf,
+                      zero_padding_factor=padding_factor,
+                      interp_factor=interp_factor)[0,...,0]
+
+  # Add noise
   image = profile + noise
   return image
 
 def main(_):
-  stamp_size = 55
+  stamp_size = 56
   N = 5
   sigma_e = 0.003
   plot = np.zeros((N*stamp_size, N*stamp_size))
