86a2da0f1c2ed26b46a26b00b905189fa77e7e4f
Benjamin Remy <remy.benjamin.101@gmail.com>
2021-11-18 17:11:34 +0100

    adding a toy model matching ngmix.metacal example

---
+++ metacal0.py
@@ -0,0 +1,315 @@
+"""
+Metacalibration (https://arxiv.org/abs/1702.02600, https://arxiv.org/abs/1702.02601)
+
+In this example we perform basic metacalibration with no object detection or
+object selections
+
+Metacalibration is a method to calibrate weak lensing shear measurements.  It
+involves creating artifically sheared images. This means deconvolving,
+shearing, and reconvolving by a new function (a new PSF). The shear estimator
+is then measured on all of these images in order to form an estimate of a
+linear response (the calibration).
+
+In this example, we use a bootstrapper, which is a wraper class to run
+measurements on the object and psf.  We use simple gaussian weighted moments
+for measurement.
+
+In this example, we set two parameters for the metacal run: the psf type for
+the final PSF in the image, and the types of images to generate.  These are set
+when constructing the MetacalBootstrapper
+
+the psf
+    We deconvolve, shear the image, then reconvolve.
+
+    Setting psf='fitgauss' means we reconvolve by a round gaussian psf, based on
+    fitting the original psf with a gaussian and dilating it appropriately.
+
+    Setting psf='gauss' uses a deterministic algorithm to create a Gaussian psf
+    that is round and larger than the original.  This algorithm is a bit slower
+    and can result in a slightly noisier measurement, because it is more
+    conservative.
+
+    The default is 'gauss'
+
+the types
+    types is the types of images to produce.  Here we just use minimal set
+    of shears to speed up this example, where we only calculate the
+    response of the g1 measurement to a shear in g1
+
+        noshear: the deconvolved/reconvolved image but without shear.  This image
+          is used to measure the shear estimator and other quantities.
+        1p: sheared +g1
+        1m: sheared -g1
+            1p/1m are are used to calculate the response and selection effects.
+
+    standard default set would also includes shears in g2 (2p, 2m)
+
+This example is low noise without any blending.  It should take about a minute
+to run and get a precise final shear estimate.  You should see that the
+recovered shear is unbiased.  The printout should look something like this
+
+    > python metacal.py
+    S/N: 79381.3
+    R11: 0.343469
+    m: 4.60743e-06 +/- 0.000419315 (99.7% conf)
+    c: -8.87648e-07 +/- 4.01498e-06 (99.7% conf)
+"""
+import numpy as np
+import ngmix
+import galsim
+
+import utils
+
+# _scale = 0.03 # COSMOS pixel size in arcsec
+_scale = 0.263
+
+def main():
+    args = get_args()
+    rng = np.random.RandomState(args.seed)
+
+    # We will measure moments with a fixed gaussian weight function
+    weight_fwhm = 1.2
+    fitter = ngmix.gaussmom.GaussMom(fwhm=weight_fwhm)
+    psf_fitter = ngmix.gaussmom.GaussMom(fwhm=weight_fwhm)
+
+    # these "runners" run the measurement code on observations
+    psf_runner = ngmix.runners.PSFRunner(fitter=psf_fitter)
+    runner = ngmix.runners.Runner(fitter=fitter)
+
+    # this "bootstrapper" runs the metacal image shearing as well as both psf
+    # and object measurements
+    #
+    # We will just do R11 for simplicity and to speed up this example;
+    # typically the off diagonal terms are negligible, and R11 and R22 are
+    # usually consistent
+
+    boot = ngmix.metacal.MetacalBootstrapper(
+        runner=runner, psf_runner=psf_runner,
+        rng=rng,
+        psf=args.psf,
+        types=['noshear', '1p', '1m'],
+    )
+
+    dlist = []
+
+    data, shear_true, sigma_n = utils.load_sims(args.filename)
+
+    for i in progress(data.shape[0], miniters=10):
+        obs = make_data(data, i, sigma_n)
+
+        resdict, obsdict = boot.go(obs)
+
+        for stype, sres in resdict.items():
+            st = make_struct(res=sres, obs=obsdict[stype], shear_type=stype)
+            dlist.append(st)
+
+    print()
+
+    data = np.hstack(dlist)
+
+    w = select(data=data, shear_type='noshear')
+    w_1p = select(data=data, shear_type='1p')
+    w_1m = select(data=data, shear_type='1m')
+
+    g = data['g'][w].mean(axis=0)
+    gerr = data['g'][w].std(axis=0) / np.sqrt(w.size)
+    g1_1p = data['g'][w_1p, 0].mean()
+    g1_1m = data['g'][w_1m, 0].mean()
+    R11 = (g1_1p - g1_1m)/0.02
+
+    shear = g / R11
+    shear_err = gerr / R11
+
+    m = shear[0]/shear_true[0]-1
+    merr = shear_err[0]/shear_true[0]
+
+    s2n = data['s2n'][w].mean()
+
+    print('sigma_n: %g' % sigma_n)
+    print('Metacalibration')
+    print('----------------------')
+    print('S/N: %g' % s2n)
+    print('R11: %g' % R11)
+    print('shear', shear)
+    print('shear true', shear_true)
+    print('m: %g +/- %g (99.7%% conf)' % (m, merr*3))
+    print('c: %g +/- %g (99.7%% conf)' % (shear[1], shear_err[1]*3))
+
+
+def select(data, shear_type):
+    """
+    select the data by shear type and size
+
+    Parameters
+    ----------
+    data: array
+        The array with fields shear_type and T
+    shear_type: str
+        e.g. 'noshear', '1p', etc.
+
+    Returns
+    -------
+    array of indices
+    """
+
+    w, = np.where(
+        (data['flags'] == 0) & (data['shear_type'] == shear_type)
+    )
+    return w
+
+
+def make_struct(res, obs, shear_type):
+    """
+    make the data structure
+
+    Parameters
+    ----------
+    res: dict
+        With keys 's2n', 'e', and 'T'
+    obs: ngmix.Observation
+        The observation for this shear type
+    shear_type: str
+        The shear type
+
+    Returns
+    -------
+    1-element array with fields
+    """
+    dt = [
+        ('flags', 'i4'),
+        ('shear_type', 'U7'),
+        ('s2n', 'f8'),
+        ('g', 'f8', 2),
+        ('T', 'f8'),
+        ('Tpsf', 'f8'),
+    ]
+    data = np.zeros(1, dtype=dt)
+    data['shear_type'] = shear_type
+    data['flags'] = res['flags']
+    if res['flags'] == 0:
+        data['s2n'] = res['s2n']
+        # for moments we are actually measureing e, the elliptity
+        data['g'] = res['e']
+        data['T'] = res['T']
+    else:
+        data['s2n'] = np.nan
+        data['g'] = np.nan
+        data['T'] = np.nan
+        data['Tpsf'] = np.nan
+
+        # we only have one epoch and band, so we can get the psf T from the
+        # observation rather than averaging over epochs/bands
+        data['Tpsf'] = obs.psf.meta['result']['T']
+
+    return data
+
+cat = galsim.COSMOSCatalog()
+
+#def make_data(stamp, rng, noise, shear):
+def make_data(data, i, sigma_n):
+    """
+    simulate an exponential object with moffat psf
+
+    Parameters
+    ----------
+    rng: np.random.RandomState
+        The random number generator
+    noise: float
+        Noise for the image
+    shear: (g1, g2)
+        The shear in each component
+
+    Returns
+    -------
+    ngmix.Observation
+    """
+    scale = _scale
+
+    # psf = cat.makeGalaxy(2,  gal_type='real', noise_pad_size=0).original_psf
+    psf_fwhm = 0.9
+    psf = galsim.Moffat(
+            beta=2.5, fwhm=psf_fwhm,
+        ).shear(
+            g1=0.02,
+            g2=-0.01,
+        )
+    
+    dx, dy = 0., 0.
+
+    # import here simulations
+    im = data[i,...]
+
+    noise = sigma_n
+    psf_noise = 1.0e-6
+
+    psf_im = psf.drawImage(scale=scale).array
+    im += np.random.normal(scale=noise, size=im.shape)
+
+    cen = (np.array(im.shape)-1.0)/2.0
+    psf_cen = (np.array(psf_im.shape)-1.0)/2.0
+
+    jacobian = ngmix.DiagonalJacobian(
+        row=cen[0] + dy/scale, col=cen[1] + dx/scale, scale=scale,
+    )
+    psf_jacobian = ngmix.DiagonalJacobian(
+        row=psf_cen[0], col=psf_cen[1], scale=scale,
+    )
+
+    wt = im*0 + 1.0/noise**2
+    psf_wt = psf_im*0 + 1.0/psf_noise**2
+
+    psf_obs = ngmix.Observation(
+        psf_im,
+        weight=psf_wt,
+        jacobian=psf_jacobian,
+    )
+
+    obs = ngmix.Observation(
+        im,
+        weight=wt,
+        jacobian=jacobian,
+        psf=psf_obs,
+    )
+
+    return obs
+
+
+def progress(total, miniters=1):
+    last_print_n = 0
+    last_printed_len = 0
+    sl = str(len(str(total)))
+    mf = '%'+sl+'d/%'+sl+'d %3d%%'
+    for i in range(total):
+        yield i
+
+        num = i+1
+        if i == 0 or num == total or num - last_print_n >= miniters:
+            meter = mf % (num, total, 100*float(num) / total)
+            nspace = max(last_printed_len-len(meter), 0)
+
+            print('\r'+meter+' '*nspace, flush=True, end='')
+            last_printed_len = len(meter)
+            if i > 0:
+                last_print_n = num
+
+    print(flush=True)
+
+
+def get_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=31415,
+                        help='seed for rng')
+    parser.add_argument('--ntrial', type=int, default=1000,
+                        help='number of trials')
+    parser.add_argument('--noise', type=float, default=1.0e-6,
+                        help='noise for images')
+    parser.add_argument('--psf', default='gauss',
+                        help='psf for reconvolution')
+    parser.add_argument('--filename', default='./data/sims_toymodel1_0.fits',
+                        help='path and name of data to process')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
---
+++ toymodel0.py
@@ -0,0 +1,146 @@
+"""
+Toy model 0
+
+Matching ngmix.examples.metacal.metacal.py data
+https://github.com/esheldon/ngmix/blob/master/examples/metacal/metacal.py
+
+Exponential light profiles
+  - constant shear
+  - constant size
+  - no intrinsic e
+
+Moffat PSF
+"""
+
+import tensorflow as tf
+import edward2 as ed
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+import fnmatch
+from absl import app
+from absl import flags
+
+import utils
+
+import galsim
+import galflow
+lp = galflow.lightprofiles
+
+flags.DEFINE_integer("N", 5, "Number of stamps on x and y axes")
+flags.DEFINE_boolean("plot", True, "Should we plot the simulations?")
+flags.DEFINE_string("output_dir", "data", "Path to output simulations")
+flags.DEFINE_string("model_name", "toymodel0", "Name of the probabilistic model")
+flags.DEFINE_boolean("save", False, "Should we store the simulations?")
+flags.DEFINE_float("sigma_n", 1e-6, "Level of noise in data")
+
+_log10 = tf.math.log(10.)
+_scale = 0.263
+_pi = np.pi
+
+FLAGS = flags.FLAGS
+
+_stamp_size = 44
+
+# PSF model from galsim COSMOS catalog
+cat = galsim.COSMOSCatalog()
+psf = cat.makeGalaxy(2,  gal_type='real', noise_pad_size=0).original_psf
+
+psf_fwhm = 0.9
+psf = galsim.Moffat(
+        beta=2.5, fwhm=psf_fwhm,
+    ).shear(
+        g1=0.02,
+        g2=-0.01,
+    )
+
+interp_factor=2
+padding_factor=2
+Nk = _stamp_size*interp_factor*padding_factor
+from galsim.bounds import _BoundsI
+bounds = _BoundsI(0, Nk//2, -Nk//2, Nk//2-1)
+
+imkpsf = psf.drawKImage(bounds=bounds,
+                        scale=2.*_pi/(_stamp_size*padding_factor*_scale),
+                        recenter=False)
+kpsf = tf.cast(np.fft.fftshift(imkpsf.array.reshape(1, Nk, Nk//2+1), axes=1), tf.complex64)
+
+@tf.function
+def model(batch_size, stamp_size, shear):
+  """Toy model
+  """
+  # stamp size
+  nx = ny = stamp_size
+
+  # pixel noise std
+  sigma_n = FLAGS.sigma_n
+  noise = ed.Normal(loc=tf.zeros((batch_size, nx, ny)), scale=sigma_n)
+
+  # constant hlr
+  hlr = 0.5 * tf.ones(batch_size)
+
+  # Flux
+  F = tf.ones(batch_size)
+
+  # Generate light profile
+  profile = lp.exponential(half_light_radius=hlr, flux=F, nx=nx, ny=ny, scale=_scale)
+  ims = tf.cast(tf.reshape(profile, (batch_size,stamp_size,stamp_size,1)), tf.float32)
+
+  # constant shear
+  gamma = tf.convert_to_tensor(shear, dtype=tf.float32)
+  gamma = tf.repeat(tf.expand_dims(gamma, 0), batch_size, axis=0)
+  # Shear the image
+  tfg1 = gamma[:, 0]
+  tfg2 = gamma[:, 1]
+  ims = galflow.shear(ims, tfg1, tfg2)
+
+  profile = ims[...,0]
+  
+  # Convolve the image with the PSF
+  profile = galflow.convolve(ims, kpsf,
+                      zero_padding_factor=padding_factor,
+                      interp_factor=interp_factor)[...,0]
+
+  # Add noise
+  image = profile + noise
+
+  return image
+
+def main(_):
+  stamp_size = _stamp_size
+  N = FLAGS.N
+  batch_size = N*N
+  sigma_n = FLAGS.sigma_n
+  true_shear = [0.01, 0.0]
+
+  sims = model(batch_size, stamp_size, true_shear)
+  print(sims.shape)
+
+  if FLAGS.save:
+    file_root = "sims_"
+    file_name = file_root + FLAGS.model_name + "_" + str(len(fnmatch.filter(os.listdir(FLAGS.output_dir), file_root + FLAGS.model_name + "*"))) + ".fits"
+    file_path = os.path.join(FLAGS.output_dir, file_name)
+    #np.save(file_path, sims)
+    utils.save_sims(file_path, sims, true_shear, sigma_n)
+
+  if FLAGS.plot:
+    sims_reshaped = sims.numpy().reshape(N,N,stamp_size,stamp_size).transpose([0,2,1,3]).reshape([N*stamp_size,N*stamp_size])
+    
+    sep = dict(color='k', linestyle=':', linewidth=.5)
+    plt.figure()
+    for i in range(N):
+      if i>0:
+        plt.axvline(x=i*stamp_size, **sep)
+        plt.axhline(y=i*stamp_size, **sep)
+        plt.axvline(x=i*stamp_size, **sep)
+        plt.axhline(y=i*stamp_size, **sep)
+    
+    plt.imshow(np.arcsinh(sims_reshaped/sigma_n)*sigma_n, cmap='gray_r')
+    plt.title(r'$Arcsinh(\frac{X}{\sigma})\cdot \sigma$')
+    plt.colorbar()
+    plt.show()
+
+if __name__ == "__main__":
+  app.run(main)
