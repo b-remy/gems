93a57c00b54f86f447bf9f28a63391d01997e2b5
Benjamin Remy <remy.benjamin.101@gmail.com>
2024-02-18 04:32:45 +0100

    add gen_obs functions

---
+++ gems/gen_obs.py
@@ -0,0 +1,190 @@
+import tensorflow_hub as hub
+import tensorflow as tf
+import tensorflow_probability as tfp
+from tensorflow_probability import edward2 as ed
+import numpy as np
+tfd = tfp.distributions
+
+
+import galsim
+from galsim.bounds import _BoundsI
+
+from gems.models import sersic2morph_model, dgm2morph_model, shear_fourier, convolve_fourier, dgm_model
+from gems.ed_utils import make_value_setter, make_log_joint_fn
+
+NUM_GAL = 144 # number of galaxies
+NOISE_LEVEL = 0.01 # gaussian noise standard deviation
+STAMP_SIZE = 128 # width of a postage stamp in pixels
+PIXEL_SCALE = 0.03 # pixel size in arcsec
+
+def gpsf2ikpsf(psf, interp_factor, padding_factor, stamp_size, im_scale):
+  Nk = stamp_size*interp_factor*padding_factor
+  bounds = _BoundsI(-Nk//2, Nk//2-1, -Nk//2, Nk//2-1)
+  imkpsf = psf.drawKImage(bounds=bounds,
+                    scale=2.*np.pi/(stamp_size*padding_factor* im_scale),
+                    recenter=False)
+  # imkpsf = tf.reshape(tf.convert_to_tensor(imkpsf.array, tf.complex64), [1, Nk, Nk])
+  imkpsf = imkpsf.array.reshape([1, Nk, Nk])
+  return imkpsf
+
+def intertwine(img1, img2):
+    """
+    Args: [NUM_IMAGES, WIDTH, HEIGHT]
+    Return: [NUM_IMAGES*2, WIDTH, HEIGHT]
+    """
+    NUM_IMAGES = img1.shape[0]
+    
+    img1 = tf.expand_dims(img1, 0)
+    img2 = tf.expand_dims(img2, 0)
+
+    pos_orig = tf.cast(tf.expand_dims(tf.one_hot(indices=tf.range(NUM_IMAGES)*2, depth=NUM_IMAGES*2),0), img1.dtype)
+    pos_rot = tf.cast(tf.expand_dims(tf.one_hot(indices=tf.range(NUM_IMAGES)*2+1, depth=NUM_IMAGES*2),0), img1.dtype)
+    
+    img1_reshape = tf.transpose(img1, [0, 2, 3, 1])
+    img2_reshape = tf.transpose(img2, [0, 2, 3, 1])
+    
+    ims_tot = tf.einsum('...i,...ik', img1_reshape, pos_orig)
+    ims_tot = ims_tot + tf.einsum('...i,...ik', img2_reshape, pos_rot)
+    
+    ims_tot = tf.transpose(ims_tot, [0, 3, 1, 2])
+    return ims_tot[0]
+
+def double_rotate_images(images):
+    """
+    Args: [batch, NUM_IMAGES, WIDTH, HEIGHT]
+    Return: [batch, NUM_IMAGES*2, WIDTH, HEIGHT]
+    """
+    
+    NUM_IMAGES = images.shape[1]
+    
+    ims_rot = tf.reshape(images, [-1, images.shape[2], images.shape[3], 1])
+    ims_rot = tf.image.rot90(ims_rot)
+    ims_rot = tf.reshape(ims_rot, [images.shape[0], NUM_IMAGES, images.shape[2], images.shape[3]])
+    
+    pos_orig = tf.cast(tf.expand_dims(tf.one_hot(indices=tf.range(NUM_IMAGES)*2, depth=NUM_IMAGES*2),0), images.dtype)
+    pos_rot = tf.cast(tf.expand_dims(tf.one_hot(indices=tf.range(NUM_IMAGES)*2+1, depth=NUM_IMAGES*2),0), images.dtype)
+    
+    ims_reshape = tf.transpose(images, [0, 2, 3, 1])
+    ims_rot_reshape = tf.transpose(ims_rot, [0, 2, 3, 1])
+    
+    ims_tot = tf.einsum('...i,...ik', ims_reshape, pos_orig)
+    ims_tot = ims_tot + tf.einsum('...i,...ik', ims_rot_reshape, pos_rot)
+    
+    ims_tot = tf.transpose(ims_tot, [0, 3, 1, 2])
+    
+    return ims_tot
+
+def gen_obs(NUM_GAL, NOISE_LEVEL, STAMP_SIZE, PIXEL_SCALE, max_hlr=0.6, g1=None, g2=None):
+
+    # Priors
+    std_gamma = 0.15
+    prior_gamma = tfd.Normal(loc=tf.zeros(2), scale=std_gamma*tf.ones(2))
+    prior_z = tfd.Normal(loc=tf.zeros(16), scale=tf.ones(16))
+
+    N = int(np.sqrt(NUM_GAL))
+    # Load auto encoder weights
+    encoder = hub.Module('../deep_galaxy_models/modules/vae_16/encoder')
+    decoder = hub.Module('../deep_galaxy_models/modules/vae_16/decoder')
+    code = hub.Module('../deep_galaxy_models/modules/latent_maf_16/code_sampler')
+    
+    cat = galsim.COSMOSCatalog(sample='25.2',
+                               max_hlr=max_hlr,
+                               dir='/linkhome/rech/genpuc01/utb76xl/shear-projects/shear-projects/gems/paper/galsim_catalog/COSMOS_25.2_training_sample')
+    
+    # select random cosmos galaxies indices
+    gal_index = cat.selectRandomIndex(NUM_GAL//2)
+    
+    # Parameters to draw the PSF
+    interp_factor=2
+    padding_factor=1
+    Nk = STAMP_SIZE*interp_factor*padding_factor
+    bounds = _BoundsI(0, Nk//2, -Nk//2, Nk//2-1)    
+
+    # Generate observations
+    im_real_list = []
+    im_psf_list = []
+    psfs = []
+    psfs2 = []
+
+    mag_auto_list = []
+    z_phot_list = []
+    flux_radius_list = []
+
+    indices = []
+    degrees = galsim.AngleUnit(np.pi / 180.)
+    angle = galsim.Angle(90, unit=degrees)
+
+    # sample theta from priors
+    if g1==None and g2==None:
+      gamma_samples = prior_gamma.sample(1)
+    else:
+      gamma_samples = tf.expand_dims(tf.cast(tf.convert_to_tensor([g1, g2]), tf.float32), 0)
+      
+    z_samples = prior_z.sample(NUM_GAL//2)
+
+    for i in range(NUM_GAL//2):
+        ind = gal_index[i]
+        
+        galr= cat.makeGalaxy(ind, gal_type='real', noise_pad_size=0.8*PIXEL_SCALE*STAMP_SIZE)
+
+        psf = galr.original_psf
+        psf2 = psf.rotate(angle)
+
+        # PSF for reconvolution
+        imkpsf = gpsf2ikpsf(psf=psf, interp_factor=1, padding_factor=1, stamp_size=STAMP_SIZE, im_scale=PIXEL_SCALE)
+        imkpsf2 = gpsf2ikpsf(psf=psf2, interp_factor=1, padding_factor=1, stamp_size=STAMP_SIZE, im_scale=PIXEL_SCALE)
+
+        #store PSF and rotated PSF
+        psfs.append(imkpsf)
+        psfs2.append(imkpsf2)
+        indices.append(ind)
+
+        mag_auto_list.append(cat.param_cat['mag_auto'][cat.orig_index[ind]])
+        z_phot_list.append(cat.param_cat['zphot'][cat.orig_index[ind]])
+        flux_radius_list.append(cat.param_cat['flux_radius'][cat.orig_index[ind]])
+
+    mag_auto_g = tf.convert_to_tensor(mag_auto_list)
+    z_phot_g = tf.convert_to_tensor(z_phot_list)
+    flux_radius_g = tf.convert_to_tensor(flux_radius_list)
+
+    imkpsfs = tf.cast(tf.concat(psfs, axis=0), tf.complex64)
+    imkpsfs2 = tf.cast(tf.concat(psfs2, axis=0), tf.complex64)
+
+    # get random code
+    z = code({'mag_auto':mag_auto_g, 
+                'flux_radius':flux_radius_g, 
+                'zphot':z_phot_g , 
+                'random_normal':z_samples})
+
+    reconstruction = decoder(z)
+
+    ims = tf.reshape(reconstruction, (1, NUM_GAL//2, STAMP_SIZE, STAMP_SIZE))
+
+    g1 = gamma_samples[:,0]
+    g2 = gamma_samples[:,1]
+    
+    im_sheared = shear_fourier(ims, g1, g2)
+
+    ims = convolve_fourier(im_sheared, imkpsfs)
+    
+    # double and rotate each galaxy
+    ims_tot = double_rotate_images(ims)
+    
+    obs = ims_tot + tf.random_normal([1, NUM_GAL, STAMP_SIZE, STAMP_SIZE]) * NOISE_LEVEL
+
+    imkpsfs = intertwine(imkpsfs, imkpsfs2)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        ouput = sess.run([
+            obs,
+            gamma_samples,
+            z_samples,
+            imkpsfs,
+        ])
+    
+    y, gamma_samples, z_samples, imkpsfs = ouput
+    
+    # return observations, parameters and catalog selection
+    return y, gamma_samples, z_samples, indices, imkpsfs, mag_auto_list, z_phot_list, flux_radius_list
--- paper/scripts/sampling_lvm.py
+++ paper/scripts/sampling_lvm.py
@@ -22,6 +22,7 @@ import matplotlib.pyplot as plt
 
 from gems.models import sersic2morph_model, dgm2morph_model, shear_fourier, convolve_fourier, dgm_model
 from gems.ed_utils import make_value_setter, make_log_joint_fn
+from gems.gen_obs import gen_obs
 
 NUM_GAL = 144 # number of galaxies
 NOISE_LEVEL = 0.01 # gaussian noise standard deviation
@@ -29,120 +30,20 @@ STAMP_SIZE = 128 # width of a postage stamp in pixels
 PIXEL_SCALE = 0.03 # pixel size in arcsec
 N = int(np.sqrt(NUM_GAL))
 
-# Load auto encoder weights
-encoder = hub.Module('../deep_galaxy_models/modules/vae_16/encoder')
-decoder = hub.Module('../deep_galaxy_models/modules/vae_16/decoder')
-
-def gpsf2ikpsf(psf, interp_factor, padding_factor, stamp_size, im_scale):
-  Nk = stamp_size*interp_factor*padding_factor
-  bounds = _BoundsI(-Nk//2, Nk//2-1, -Nk//2, Nk//2-1)
-  imkpsf = psf.drawKImage(bounds=bounds,
-                    scale=2.*np.pi/(stamp_size*padding_factor* im_scale),
-                    recenter=False)
-  # imkpsf = tf.reshape(tf.convert_to_tensor(imkpsf.array, tf.complex64), [1, Nk, Nk])
-  imkpsf = imkpsf.array.reshape([1, Nk, Nk])
-  return imkpsf
-
-cat = galsim.COSMOSCatalog(sample='25.2',
-                            dir='/linkhome/rech/genpuc01/utb76xl/shear-projects/shear-projects/gems/paper/galsim_catalog/COSMOS_25.2_training_sample')
-
-interp_factor=2
-padding_factor=1
-Nk = STAMP_SIZE*interp_factor*padding_factor
-bounds = _BoundsI(0, Nk//2, -Nk//2, Nk//2-1)    
-
-begin = time.time()
-
-
-# Generate observations
-im_real_list = []
-im_psf_list = []
-psfs = []
-
-mag_auto_list = []
-z_phot_list = []
-flux_radius_list = []
-
-indices = []
-degrees = galsim.AngleUnit(np.pi / 180.)
-angle = galsim.Angle(90, unit=degrees)
-
-ind = 0
-while len(im_real_list) < NUM_GAL:
-    galp = cat.makeGalaxy(ind, gal_type='parametric')
-    if cat.param_cat['use_bulgefit'][cat.orig_index[ind]] == 0:
-        if galp.original.n < .4 or galp.original.half_light_radius > 3. or cat.param_cat['mag_auto'][cat.orig_index[ind]] < 22.5:
-            ind += 1
-        else:
-            galp = cat.makeGalaxy(ind, gal_type='parametric')
-            im_real = galsim.ImageF(STAMP_SIZE, STAMP_SIZE, scale=PIXEL_SCALE)
-            galr= cat.makeGalaxy(ind, gal_type='real', noise_pad_size=0.8*PIXEL_SCALE*STAMP_SIZE)
-            psf = galr.original_psf
-
-            if indices.count(ind)==1:
-                galr = galr.rotate(angle)
-                psf = psf.rotate(angle)
-
-            real = galsim.Convolve(psf, galr)
-            real.drawImage(im_real, method='no_pixel', use_true_center=False)
-
-            # PSF for the autocoder
-            imCp = psf.drawKImage(bounds=bounds,
-                                    scale=2.*np.pi/(Nk * PIXEL_SCALE / interp_factor),
-                                    recenter=False)
-            im_psf = np.abs(np.fft.fftshift(imCp.array, axes=0)).astype('float32')
-
-            # PSF for reconvolution
-            imkpsf = gpsf2ikpsf(psf=psf, interp_factor=1, padding_factor=1, stamp_size=STAMP_SIZE, im_scale=PIXEL_SCALE)
-            psfs.append(imkpsf)
-
-            im_real_list.append(im_real.array)
-            im_psf_list.append(im_psf)
-            indices.append(ind)
-
-            mag_auto_list.append(cat.param_cat['mag_auto'][cat.orig_index[ind]])
-            z_phot_list.append(cat.param_cat['zphot'][cat.orig_index[ind]])
-            flux_radius_list.append(cat.param_cat['flux_radius'][cat.orig_index[ind]])
-
-            print(ind, len(im_real_list))
-
-            if indices.count(ind)==2:
-                ind += 1
-    else:
-        ind += 1
-
-im_real_list = np.stack(im_real_list, axis=0)
-im_psf_list = np.stack(im_psf_list, axis=0)
-
-imkpsfs = tf.cast(tf.concat(psfs, axis=0), tf.complex64)
-
-psf_in = tf.placeholder(shape=[NUM_GAL, 256, 129, 1], dtype=tf.float32)
-im_in = tf.placeholder(shape=[NUM_GAL, 128, 128, 1], dtype=tf.float32)
-
-code = encoder({'input':im_in, 'psf':psf_in})
-reconstruction = decoder(code)
-
-ims = tf.reshape(reconstruction, (1, NUM_GAL, STAMP_SIZE, STAMP_SIZE))
-
 g1 = -0.03
 g2 = +0.03
-im_sheared = shear_fourier(ims, g1, g2)
 
-ims = convolve_fourier(im_sheared, imkpsfs)
-
-obs = ims + tf.random_normal([1, NUM_GAL, STAMP_SIZE, STAMP_SIZE]) * NOISE_LEVEL
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-
-    y = sess.run(obs, feed_dict={psf_in:im_psf_list.reshape((NUM_GAL,256,129,1)), im_in:im_real_list.reshape((NUM_GAL,128,128,1))})
-    #np.save('/content/drive/MyDrive/GEMS/obs16AE.npy', y)
-
-    im_obs = y.reshape(N,N,STAMP_SIZE,STAMP_SIZE).transpose([0,2,1,3]).reshape([N*(STAMP_SIZE),N*(STAMP_SIZE)])
-    k = 10
-    obs_cropped = y[:,:,k:-k, k:-k]
+out = gen_obs(NUM_GAL, NOISE_LEVEL, STAMP_SIZE, PIXEL_SCALE, max_hlr=0.6, g1=g1, g2=g2)
     
-    
+y, true_gamma, z_samples, indices, imkpsfs, mag_auto_list, z_phot_list, flux_radius_list = out
+
+mag_auto_list = np.repeat(np.array(mag_auto_list, np.float32), 2)
+z_phot_list = np.repeat(np.array(z_phot_list, np.float32), 2)
+flux_radius_list = np.repeat(np.array(flux_radius_list, np.float32), 2)
+
+k = 10
+obs_cropped = y[:,:,k:-k, k:-k]
+
 #TODO: does not work with batch size > 1 yet...
 batch_size = 2
 obs_ = tf.repeat(obs_cropped, repeats=batch_size, axis=0)
@@ -152,13 +53,11 @@ log_prob = make_log_joint_fn(partial(dgm_model,
                                   sigma_e=NOISE_LEVEL, 
                                   stamp_size=STAMP_SIZE,
                                   num_gal=N*N, 
-                                  kpsf=imkpsfs, 
-                                  interp_factor = interp_factor,
-                                  padding_factor = padding_factor,
-                                  mag_auto_list=mag_auto_list, 
-                                  z_phot_list=z_phot_list, 
-                                  flux_radius_list=flux_radius_list,
-                                  fit_centroid=False))
+                                  kpsf=imkpsfs,
+                                  mag_auto_list= mag_auto_list, 
+                                  z_phot_list = z_phot_list, 
+                                  flux_radius_list = flux_radius_list,
+                                  fit_centroid = False))
 
 s_gamma = .05
 def target_log_prob_fn(prior_z, gamma):
@@ -221,7 +120,7 @@ def get_samples():
   
 samples, trace = get_samples()
 
-true_gamma = tf.expand_dims(tf.convert_to_tensor([g1, g2]), 0)
+true_gamma = tf.cast(tf.convert_to_tensor(true_gamma), tf.float32)
 
 lz_samples = samples[0]
 gamma_samples = samples[1]*s_gamma
@@ -237,6 +136,10 @@ end = time.time()
 
 print((end - start)/60)
 
-np.save('./paper/scripts/results/gamma', gamma_samples_)
-np.save('./paper/scripts/results/lz', lz_samples_)
-np.save('./paper/scripts/results/im_obs', im_obs)
+np.save('./paper/scripts/results/mcmc_init/gamma.npy', gamma_samples_)
+np.save('./paper/scripts/results/mcmc_init/lz.npy', lz_samples_)
+np.save('./paper/scripts/results/mcmc_init/y.npy', y)
+np.save('./paper/scripts/results/mcmc_init/imkpsfs.npy', imkpsfs)
+np.save('./paper/scripts/results/mcmc_init/mag_auto_list.npy', mag_auto_list)
+np.save('./paper/scripts/results/mcmc_init/z_phot_list.npy', z_phot_list)
+np.save('./paper/scripts/results/mcmc_init/flux_radius_list.npy', flux_radius_list)
