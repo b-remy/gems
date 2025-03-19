312b42fe65f3d814fd89d41becdd16ca0f062091
Benjamin Remy <remy.benjamin.101@gmail.com>
2025-03-19 16:03:02 -0400

    rounds of sampling

--- paper/scripts/sampling_lvm_no_pairs.py
+++ paper/scripts/sampling_lvm_no_pairs.py
@@ -1,5 +1,5 @@
-#from absl import app
-#from absl import flags
+from absl import app
+from absl import flags
 
 import os
 import tensorflow_hub as hub
@@ -22,6 +22,17 @@ from gems.models import sersic2morph_model, dgm2morph_model, shear_fourier, conv
 from gems.ed_utils import make_value_setter, make_log_joint_fn
 from gems.gen_obs import gen_obs_no_pairs
 
+from pathlib import Path
+PATH = Path('.')
+PATH.mkdir(parents=True, exist_ok=True)
+
+flags.DEFINE_integer("num_samples", 10, "Number of mcmc samples per chain")
+flags.DEFINE_string("experiment_folder", "unnormalized", "Experiement folder")
+flags.DEFINE_integer("round", 0, "Sampling round")
+flags.DEFINE_integer("batch_size", 2, "Number of chains in parallel")
+flags.DEFINE_float("gamma_rescale", 0.1, "Number of chains in parallel")
+
+
 NUM_GAL = 144 # number of galaxies
 NOISE_LEVEL = 0.01 # gaussian noise standard deviation
 STAMP_SIZE = 128 # width of a postage stamp in pixels
@@ -31,126 +42,145 @@ N = int(np.sqrt(NUM_GAL))
 g1 = -0.05
 g2 = +0.05
 
-out = gen_obs_no_pairs(NUM_GAL, NOISE_LEVEL, STAMP_SIZE, PIXEL_SCALE, max_hlr=0.5, max_flux=150, g1=g1, g2=g2)
+FLAGS = flags.FLAGS
+
+def main(_):
+
+    if FLAGS.round==0:
+        out = gen_obs_no_pairs(NUM_GAL, NOISE_LEVEL, STAMP_SIZE, PIXEL_SCALE, max_hlr=0.5, max_flux=150, g1=g1, g2=g2)            
     
-y, true_gamma, z_samples, indices, imkpsfs, mag_auto_list, z_phot_list, flux_radius_list = out
-
-#mag_auto_list = np.repeat(np.array(mag_auto_list, np.float32), 2)
-#z_phot_list = np.repeat(np.array(z_phot_list, np.float32), 2)
-#flux_radius_list = np.repeat(np.array(flux_radius_list, np.float32), 2)
-
-mag_auto_list = np.array(mag_auto_list, np.float32)
-z_phot_list = np.array(z_phot_list, np.float32)
-flux_radius_list = np.array(flux_radius_list, np.float32)
-
-k = 10
-obs_cropped = y[:,:,k:-k, k:-k]
-
-#TODO: does not work with batch size > 1 yet...
-batch_size = 2
-obs_ = tf.repeat(obs_cropped, repeats=batch_size, axis=0)
-
-log_prob = make_log_joint_fn(partial(dgm_model, 
-                                  batch_size=batch_size, 
-                                  sigma_e=NOISE_LEVEL, 
-                                  stamp_size=STAMP_SIZE,
-                                  num_gal=N*N, 
-                                  kpsf=imkpsfs,
-                                  mag_auto_list= mag_auto_list, 
-                                  z_phot_list = z_phot_list, 
-                                  flux_radius_list = flux_radius_list,
-                                  fit_centroid = False))
-
-s_gamma = .1
-def target_log_prob_fn(prior_z, gamma):
-    return log_prob(
-      prior_z=prior_z,
-      gamma=gamma*s_gamma,
-      obs=obs_)
-
-def loss_fn(lz, gamma):
-    return - target_log_prob_fn(lz, gamma)
-  
-lz = tf.Variable(tf.zeros([batch_size, NUM_GAL,16]), trainable=True, dtype=tf.float32)
-gamma = tf.Variable(tf.zeros((batch_size, 2)), trainable=True, dtype=tf.float32)
-
-loss = loss_fn(lz, gamma)
-
-# Define the optimizer
-optimizer = tf.train.AdagradOptimizer(learning_rate=0.05)
-train = optimizer.minimize(loss, var_list=[lz, gamma])
-
-# initialize the variables
-init = tf.global_variables_initializer()
-with tf.Session() as sess:
-    sess.run(init)
-
-    losses = []
-    for i in tqdm(range(700)):
-    #for i in tqdm(range(1)):
-        _, l, lz_, g_ = sess.run([train, loss, lz, gamma])
-        losses.append(l)
-
-print('g_MAP:', g_*s_gamma)
-
-# Initialize the HMC transition kernel.
-#num_results = int(100)
-#num_burnin_steps = int(100)
-
-num_results = int(20_000)
-num_burnin_steps = int(500)
-
-#num_results = int(2)
-#num_burnin_steps = int(1)
-
-adaptive_hmc = tfp.mcmc.SimpleStepSizeAdaptation(
-    tfp.mcmc.HamiltonianMonteCarlo(
-        target_log_prob_fn=target_log_prob_fn,
-        num_leapfrog_steps=3,
-        step_size=.01),
-    num_adaptation_steps=int(num_burnin_steps * 0.8))
-
-def get_samples():
-    samples, trace = tfp.mcmc.sample_chain(
-        num_results=num_results,
-        num_burnin_steps=num_burnin_steps,
-        current_state=[
-                      lz_,
-                      g_,
-        ],
-        kernel=adaptive_hmc,
-        trace_fn=lambda _, pkr: [pkr.inner_results.accepted_results.step_size,
-                                 pkr.inner_results.is_accepted]
-    )
-    return samples, trace
-  
-samples, trace = get_samples()
-
-true_gamma = tf.cast(tf.convert_to_tensor(true_gamma), tf.float32)
-
-lz_samples = samples[0]
-gamma_samples = samples[1]*s_gamma
-gamma_true = true_gamma[0,:]
-
-start = time.time()
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    #lz_samples_, gamma_samples_, gamma_true_ = sess.run([lz_samples, gamma_samples, gamma_true])
-    lz_samples_, gamma_samples_, gamma_true_, trace_sz, trace_ia = sess.run([lz_samples, gamma_samples, gamma_true, trace[0], trace[1]])
+        y, true_gamma, z_samples, indices, imkpsfs, mag_auto_list, z_phot_list, flux_radius_list = out
     
-end = time.time()
+        mag_auto_list = np.array(mag_auto_list, np.float32)
+        z_phot_list = np.array(z_phot_list, np.float32)
+        flux_radius_list = np.array(flux_radius_list, np.float32)
 
-print((end - start)/60)
+    else:
+        folder_path = os.path.join(FLAGS.experiment_folder, f"{FLAGS.round-1}")
 
-folder = "test"
+        y = np.load(os.path.join(folder_path), 'y.npy')
+        step_size = np.load(os.path.join(folder_path), 'last_step_size.npy')[0]
+        g_init = np.load(os.path.join(folder_path), 'gamma.npy')[-1]
+        z_init = np.load(os.path.join(folder_path), 'lz.npy')[-1]
+        imkpsfs = np.load(os.path.join(folder_path), 'imkpsfs.npy')
+        mag_auto_list = np.load(os.path.join(folder_path), 'mag_auto_list.npy')
+        z_phot_list = np.load(os.path.join(folder_path), 'z_phot_list.npy')
+        flux_radius_list = np.load(os.path.join(folder_path), 'flux_radius_list.npy')
 
-np.save('./{}/gamma.npy'.format(folder), gamma_samples_)
-np.save('./{}/lz.npy'.format(folder), lz_samples_)
-np.save('./{}/y.npy'.format(folder), y)
-np.save('./{}/imkpsfs.npy'.format(folder), imkpsfs)
-np.save('./{}/mag_auto_list.npy'.format(folder), mag_auto_list)
-np.save('./{}/z_phot_list.npy'.format(folder), z_phot_list)
-np.save('./{}/flux_radius_list.npy'.format(folder), flux_radius_list)
-np.save('./{}/trace_ia.npy'.format(folder), trace_ia)
-np.save('./{}/last_step_size.npy'.format(folder), trace_sz[-1:])
+
+    k = 10
+    obs_cropped = y[:,:,k:-k, k:-k]
+
+    #TODO: does not work with batch size > 1 yet...
+    batch_size = FLAGS.batch_size
+    obs_ = tf.repeat(obs_cropped, repeats=batch_size, axis=0)
+
+    log_prob = make_log_joint_fn(partial(dgm_model, 
+                                    batch_size=batch_size, 
+                                    sigma_e=NOISE_LEVEL, 
+                                    stamp_size=STAMP_SIZE,
+                                    num_gal=N*N, 
+                                    kpsf=imkpsfs,
+                                    mag_auto_list= mag_auto_list, 
+                                    z_phot_list = z_phot_list, 
+                                    flux_radius_list = flux_radius_list,
+                                    fit_centroid = False))
+
+    s_gamma = FLAGS.gamma_rescale
+
+    def target_log_prob_fn(prior_z, gamma):
+        return log_prob(
+        prior_z=prior_z,
+        gamma=gamma*s_gamma,
+        obs=obs_)
+
+    def loss_fn(lz, gamma):
+        return - target_log_prob_fn(lz, gamma)
+    
+    if FLAGS.round==0:
+        # Find MAP
+        lz = tf.Variable(tf.zeros([batch_size, NUM_GAL,16]), trainable=True, dtype=tf.float32)
+        gamma = tf.Variable(tf.zeros((batch_size, 2)), trainable=True, dtype=tf.float32)
+
+        loss = loss_fn(lz, gamma)
+
+        # Define the optimizer
+        optimizer = tf.train.AdagradOptimizer(learning_rate=0.05)
+        train = optimizer.minimize(loss, var_list=[lz, gamma])
+
+        # initialize the variables
+        init = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            sess.run(init)
+
+            losses = []
+            for i in tqdm(range(700)):
+            #for i in tqdm(range(1)):
+                _, l, lz_, g_ = sess.run([train, loss, lz, gamma])
+                losses.append(l)
+
+            z_init = lz_
+            g_init = g_ * s_gamma
+
+        print('g_MAP:', g_ * s_gamma)
+
+        # num_burnin_steps = int(500)
+        num_burnin_steps = int(1)
+    else:
+        num_burnin_steps = int(0)
+
+    num_results = int(FLAGS.num_samples)
+
+    adaptive_hmc = tfp.mcmc.SimpleStepSizeAdaptation(
+        tfp.mcmc.HamiltonianMonteCarlo(
+            target_log_prob_fn=target_log_prob_fn,
+            num_leapfrog_steps=3,
+            step_size=.01),
+        num_adaptation_steps=int(num_burnin_steps * 0.8))
+
+    def get_samples():
+        samples, trace = tfp.mcmc.sample_chain(
+            num_results=num_results,
+            num_burnin_steps=num_burnin_steps,
+            current_state=[
+                        z_init,
+                        g_init / s_gamma,
+            ],
+            kernel=adaptive_hmc,
+            trace_fn=lambda _, pkr: [pkr.inner_results.accepted_results.step_size,
+                                    pkr.inner_results.is_accepted]
+        )
+        return samples, trace
+    
+    samples, trace = get_samples()
+
+    true_gamma = tf.cast(tf.convert_to_tensor(true_gamma), tf.float32)
+
+    lz_samples = samples[0]
+    gamma_samples = samples[1]*s_gamma
+    gamma_true = true_gamma[0,:]
+
+    start = time.time()
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        lz_samples_, gamma_samples_, gamma_true_, trace_sz, trace_ia = sess.run([lz_samples, gamma_samples, gamma_true, trace[0], trace[1]])
+        
+    end = time.time()
+
+    print((end - start)/60)
+
+    save_folder = os.path.join(FLAGS.experiment_folder, f"{FLAGS.round}")
+
+    np.save(os.path.join(save_folder, 'gamma.npy', gamma_samples_))
+    np.save(os.path.join(save_folder, 'lz.npy', lz_samples_))
+    np.save(os.path.join(save_folder, 'y.npy', y))
+    np.save(os.path.join(save_folder, 'imkpsfs.npy', imkpsfs))
+    np.save(os.path.join(save_folder, 'mag_auto_list.npy', mag_auto_list))
+    np.save(os.path.join(save_folder, 'z_phot_list.npy', z_phot_list))
+    np.save(os.path.join(save_folder, 'flux_radius_list.npy', flux_radius_list))
+    np.save(os.path.join(save_folder, 'trace_ia.npy', trace_ia))
+    np.save(os.path.join(save_folder, 'last_step_size.npy', trace_sz[-1:]))
+
+if __name__=='__main__':
+    app.run(main)
